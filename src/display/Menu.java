3f49bb88623856672f9c592c5e7e9341753e3070
Jordan Aranda Tejada <jordan.aranda@me.com>
2013-03-13 17:52:49 +0100

    Cambio de evento en el menu del creador de lang

--- src/display/Menu.java
+++ src/display/Menu.java
@@ -37,7 +37,7 @@ public class Menu extends JMenuBar implements ActionListener
 		edit = new JMenu("Edit");
 		edit.setMargin(new Insets(5, 5, 5, 5));
 
-		help = new JMenuItem("Edit");
+		help = new JMenuItem("Help");
 		help.setMargin(new Insets(5, 5, 5, 5));
 		help.addActionListener(new ActionListener()
 		{
@@ -50,27 +50,8 @@ public class Menu extends JMenuBar implements ActionListener
 
 		open = new JMenuItem("Open");
 		open.setMargin(new Insets(5, 5, 5, 5));
-		open.addActionListener(new ActionListener()
-		{
-			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				System.out.println("1");
-				try{
-					FileInputStream fis = new FileInputStream("/Users/jordanarandatejada/Documents/workspace/Animature_Map/lang/es_ES.lang");
-					ObjectInputStream ois = new ObjectInputStream(fis);
-					@SuppressWarnings("unchecked")
-					HashMap<String,String> hashMap = (HashMap<String, String>)ois.readObject();
-					ois.close();
-					LangEditor lEdit = new LangEditor(hashMap);
-					Window.getInstance().setContentPane(lEdit);
-					lEdit.updateUI();
-				}catch(Exception ex){
-					System.out.println("Error al abrir el fichero");
-				}
-				System.out.println("2");
-			}
-		});
+		open.addActionListener(this);
+		
 		save = new JMenuItem("Save");
 		save.setMargin(new Insets(5, 5, 5, 5));
 		save_where = new JMenuItem("Save in...");
@@ -91,12 +72,14 @@ public class Menu extends JMenuBar implements ActionListener
 		if(e.getSource()==open)
 		{
 			try{
-				FileInputStream fis = new FileInputStream("/Users/jordanarandatejada/Documents/workspace/Animature_Map/lang/es_ES.lang");
+				FileInputStream fis = new FileInputStream("lang/es_ES.lang");
 				ObjectInputStream ois = new ObjectInputStream(fis);
 				@SuppressWarnings("unchecked")
 				HashMap<String,String> hashMap = (HashMap<String, String>)ois.readObject();
 				ois.close();
-				Window.getInstance().setContentPane(new LangEditor(hashMap));
+				LangEditor lEdit = new LangEditor(hashMap);
+				Window.getInstance().setContentPane(lEdit);
+				lEdit.updateUI();
 			}catch(Exception ex){
 				System.out.println("Error al abrir el fichero");
 			}
