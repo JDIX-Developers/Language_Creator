3bf56f4c512fe761f0caafb6bf70996782e3db19
Razican <admin@razican.com>
2013-03-18 17:12:38 +0100

    Se borra una escritura a archivo innecesaria.

--- src/utils/FileMode.java
+++ src/utils/FileMode.java
@@ -8,60 +8,75 @@ import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class FileMode 
-{
-	public static File openFileMode(String description, String...extensions)
+public class FileMode {
+
+	public static File openFileMode(String description, String ... extensions)
 	{
 		JFileChooser fileChooser = new JFileChooser();
-		FileNameExtensionFilter langFilter = new FileNameExtensionFilter(description, extensions);
+		FileNameExtensionFilter langFilter = new FileNameExtensionFilter(
+		description, extensions);
 		fileChooser.setFileFilter(langFilter);
 		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 		String path = "";
 		File file = null;
-		try{
+		try
+		{
 			if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
 			{
 				path = fileChooser.getSelectedFile().getAbsolutePath();
 				file = new File(path);
 			}
-		}catch (Exception e){
+		}
+		catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 		return file;
 	}
-	public static String saveFile(Object content, String description, String extension)
+
+	public static String saveFile(Object content, String description,
+	String extension)
 	{
 		JFileChooser fileChooser = new JFileChooser();
-		FileNameExtensionFilter langFilter = new FileNameExtensionFilter(description, extension);
+		FileNameExtensionFilter langFilter = new FileNameExtensionFilter(
+		description, extension);
 		fileChooser.setFileFilter(langFilter);
 		String path = "";
-		try{ 
-			if(fileChooser.showSaveDialog(null)==JFileChooser.APPROVE_OPTION)
-			{ 
-				path = fileChooser.getSelectedFile().getAbsolutePath(); 
-				if(!path.endsWith("."+extension)){
-					path+="."+extension;
+		try
+		{
+			if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION)
+			{
+				path = fileChooser.getSelectedFile().getAbsolutePath();
+				if (!path.endsWith("." + extension))
+				{
+					path += "." + extension;
 				}
 				File file = new File(path);
-				if((file.exists() && JOptionPane.OK_OPTION == JOptionPane.showConfirmDialog(null,"The file exists, do you want to replace it?","File Exists",JOptionPane.YES_NO_OPTION)) || !file.exists())
+				if ((file.exists() && JOptionPane.OK_OPTION == JOptionPane
+				.showConfirmDialog(null,
+				"The file exists, do you want to replace it?", "File Exists",
+				JOptionPane.YES_NO_OPTION))
+				|| !file.exists())
 				{
-					ObjectOutputStream oos = new ObjectOutputStream (new FileOutputStream(path));
+					ObjectOutputStream oos = new ObjectOutputStream(
+					new FileOutputStream(path));
 					oos.writeObject(content);
-					oos.writeObject(null);
 					oos.close();
 				}
 			}
 			return path;
-		}catch (Exception e)
-		{ 
+		}
+		catch (Exception e)
+		{
 			e.printStackTrace();
 			return path;
-		} 
+		}
 	}
-	public static void main (String [] args)
+
+	public static void main(String[] args)
 	{
 		File file = FileMode.openFileMode("FCA", "fca");
 		System.out.println(file.toString());
 	}
-	
+
 }
