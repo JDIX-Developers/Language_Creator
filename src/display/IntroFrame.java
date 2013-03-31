03f744097fdfaad7b0ac017f35a9db7a90be50e4
JAT112358 <jordan.aranda@me.com>
2013-03-31 06:54:40 +0200

    Se añade intro.

--- src/display/IntroFrame.java
+++ src/display/IntroFrame.java
@@ -1,7 +1,9 @@
 package display;
 
-import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
@@ -9,28 +11,36 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 public class IntroFrame extends JFrame {
 
-	private JPanel	contentPane;
+	private PanelBackground	contentPane;
 
 	public IntroFrame()
 	{
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(0, 0, 500, 300);
 		setLocationRelativeTo(null);
 		setUndecorated(true);
-		contentPane = new JPanel();
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane = new PanelBackground("img/intro-image.png");
 		setContentPane(contentPane);
-		// AÑADIR CONTENIDO
+		getContentPane().setLayout(null);
 
+		contentPane.updateUI();
+
+		setVisible(true);
 	}
 
 	public static void main(String[] args)
 	{
-		/*
-		 * IntroFrame frame = new IntroFrame(); frame.setVisible(true); try {
-		 * Thread.sleep(5050); } catch (InterruptedException e) {
-		 * e.printStackTrace(); } frame.dispose();
-		 */
+		IntroFrame frame = new IntroFrame();
+		try
+		{
+			Thread.sleep(5100);
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+		frame.dispose();
+
 		try
 		{
 			UIManager.setLookAndFeel(utils.Preferences.getLookAndFeel());
@@ -44,5 +54,26 @@ public class IntroFrame extends JFrame {
 		Window.getInstance().setVisible(true);
 		((Start) Window.getInstance().getContentPane()).getTabbedPane()
 		.requestFocus();
+	}
+
+	class PanelBackground extends JPanel {
+
+		private ImageIcon	image;
+		private String		name;
+
+		public PanelBackground(String name)
+		{
+			this.name = name;
+		}
+
+		@Override
+		public void paint(Graphics g)
+		{
+			Dimension size = getSize();
+			image = new ImageIcon(name);
+			g.drawImage(image.getImage(), 0, 0, size.width, size.height, null);
+			setOpaque(false);
+			super.paint(g);
+		}
 	}
 }
